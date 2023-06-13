a8069f86078576c98a764cfc9d31ddf2e21875b7
Reni <r.koci98@gmail.com>
2023-06-13 11:36:03 +0200

    Added CORS configs

--- src/main/java/dev/reviews/ReviewController.java
+++ src/main/java/dev/reviews/ReviewController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
